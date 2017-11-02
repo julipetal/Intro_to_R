007623f0280728323d037fee852c38dc79608d17
Juli Petereit <julipetal@users.noreply.github.com>
2017-11-02 13:25:37 -0700

    Update script_readinFile.r

--- script_readinFile.r
+++ script_readinFile.r
@@ -10,12 +10,12 @@ fileName = ""
 
 l1 = scan(fileName, sep = "\t", what = "", nlines = 1)
 
-tmpM= matrix(scan(fileName, what="", sep="\t", skip=1), ncol=length(l1), byrow = TRUE)
-myData= matrix(as.numeric(tmpM[,-1]), ncol=length(l1) -1, byrow=FALSE)
+tmpM   = matrix(scan(fileName, what = "", sep = "\t", skip = 1), ncol = length(l1), byrow = TRUE)
+myData = matrix(as.numeric(tmpM[,-1]), ncol = length(l1)-1, byrow = FALSE)
 colnames(myData) = l1[-1]
 rownames(myData) = tmpM[,1]
 
-# remove variables (objects) you no longer need
+# remove unnecessary variables (objects) 
 rm(fileName, tmpM, l1)
 
 # check if the file was read in correctly
